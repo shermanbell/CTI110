dcf5c6fc0719f596af07dd175021b2b53612dc56
shermanbell <31597628+shermanbell@users.noreply.github.com>
2017-09-03 17:34:16 -0400

    Add files via upload

---
+++ M2HW1.py
@@ -0,0 +1,21 @@
+# CTI-110
+# M2HW1_DISTANCETRAVELED
+# SHERMAN_BELL
+# 3_SEPT_2017
+
+# A car is traveling at 70 miles per hour. Display the following:
+# The distance the car will travel in 6 hours
+# The distance the car will travel in 10 hours
+# the distance the car will travel in 15 hours
+# Use distance = speed x time
+# Hours       Distance Traveled
+#   6                420
+#   10               700
+#   15               1,050
+
+
+speed = 70
+print( "The distance the car will travel in 6 hours is " + str ((speed * 6)) + " miles")
+print( "The distance the car will travel in 10 hours is " + str ((speed * 10)) + " miles")
+print( "The distance the car will travel in 15 hours is " + str ((speed * 15)) + " miles")
+
