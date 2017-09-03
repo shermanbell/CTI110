71dece5f0bb55df341a787f0cf428d8b37a6c0ea
shermanbell <31597628+shermanbell@users.noreply.github.com>
2017-09-03 15:47:52 -0400

    Add files via upload

---
+++ Sales_prediction.py
@@ -0,0 +1,14 @@
+# CTI-110
+#M2T1-SALES PREDICTION
+#SHERMAN BELL
+# 3 Sept 2017
+# Projected total sales.
+
+
+total_sales = float (input ("Enter the Total sales: "))
+
+# calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print ("The Annual Profit is $" , format(profit, ',.2f'))
