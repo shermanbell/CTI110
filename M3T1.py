bf66f08d9bd1a2cd7a1c2f35624410df223a171f
shermanbell <31597628+shermanbell@users.noreply.github.com>
2017-09-26 22:33:34 -0400

    Add files via upload

---
+++ M3T1.py
@@ -0,0 +1,21 @@
+print #Sherman_Bell
+print #CTI_110
+print #M3T1_Areas_of_Rectangles
+print #24_Sept_2017
+
+# Areas of Rectangles
+# Write a program that asks for the length and width of two rectangles
+# The area of a rectangle is length times width
+# The program should tell the user which rectangle has the greater area or if they are equal
+
+rectangle1length = float( input( "Please enter the length of the first rectangle: ")
+rectangle1width = float( input( "Please enter the length of the first rectangle: ")
+rectangle2length = float( input( "Please enter the length of the second rectangle: ")
+rectangle2width = float( input( "Please enter the length of the second rectangle: ")
+rectangle1area = rectangle1length * rectangle1width
+rectangle2area = rectangle2length * rectangle2width
+
+if rectangle1area > rectangle2area : print( "Rectangle 1 is larger than Rectangle 2")
+elif rectangle1area < rectangle2area : print( "Rectangle 2 is larger than Rectangle 1")
+elif if rectangle1area == rectangle2area : print( "Rectangle 1 is equal to Rectangle 2")
+                          
